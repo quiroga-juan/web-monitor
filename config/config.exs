7dfb6385ace666333be6f2bf171aeec17a75e5ab
GelosoFederico <gelosofederico@gmail.com>
2021-06-20 22:50:31 -0300

    Cambiado el proyecto para que utilice Phoenix

--- config/config.exs
+++ config/config.exs
@@ -1,10 +1,31 @@
-import Config
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
 
-config :monitor, Monitor.Repo,
-  database: "monitor",
-  username: "postgres",
-  password: "Laoctava8$",
-  hostname: "localhost",
-  port: 5432
+# General application configuration
+use Mix.Config
 
-config :monitor, ecto_repos: [Monitor.Repo]
+config :monitor,
+  ecto_repos: [Monitor.Repo]
+
+# Configures the endpoint
+config :monitor, MonitorWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "o/yaGbaV36lNcTjkbZoKc55WYH7bsJR5sFfpgWO0jxDcF4GhgWNRbTQW46psv/KX",
+  render_errors: [view: MonitorWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: Monitor.PubSub,
+  live_view: [signing_salt: "/pJ+iOmz"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
--- lib/monitor/application.ex
+++ lib/monitor/application.ex
@@ -8,12 +8,28 @@ defmodule Monitor.Application do
   @impl true
   def start(_type, _args) do
     children = [
+      # Start the Ecto repository
       Monitor.Repo,
+      # Start the Telemetry supervisor
+      MonitorWeb.Telemetry,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: Monitor.PubSub},
+      # Start the Endpoint (http/https)
+      MonitorWeb.Endpoint
+      # Start a worker by calling: Monitor.Worker.start_link(arg)
+      # {Monitor.Worker, arg}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Monitor.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    MonitorWeb.Endpoint.config_change(changed, removed)
+    :ok
   end
 end
