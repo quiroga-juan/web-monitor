c98447c153ac881fd2b758721219625ad9ca4dc8
Juan Pablo Quiroga <juan.quiroga@tunubi.com>
2021-06-26 19:16:09 -0300

    Se agregan metodos para manejar las urls en la db.

--- lib/monitor/schemas/url.ex
+++ lib/monitor/schemas/url.ex
@@ -1,13 +1,22 @@
 defmodule Monitor.Url do
   use Ecto.Schema
+  alias Ecto.Changeset
 
+  @primary_key {:url_name, :string, []}
   schema "url" do
-    field :url_name, :string
+    field :url_last_status, :integer
+    timestamps()
   end
 
-  def changeset(url, params \\ %{}) do
+  def url_changeset(url, params \\ %{}) do
     url
-    |> Ecto.Changeset.cast(params, [:url_name])
-    |> Ecto.Changeset.validate_required([:url_name])
+    |> Changeset.cast(params, [:url_name])
+    |> Changeset.validate_required([:url_name])
+  end
+
+  def status_changeset(url, params \\ %{}) do
+    url
+    |> Changeset.cast(params, [:url_last_status])
+    |> Changeset.validate_required([:url_last_status])
   end
 end
